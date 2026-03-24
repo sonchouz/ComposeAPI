4457c02274170ddb308549f02070f91217b5af51
sonchouz <sofianikonorova41@gmail.com>
2026-03-24 20:46:51 +0700

    upgrade

--- app/src/main/java/com/example/composeapi/MainActivity.kt
+++ app/src/main/java/com/example/composeapi/MainActivity.kt
@@ -11,7 +11,6 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.composeapi.presentation.screens.MainScreen
 import com.example.composeapi.ui.theme.ComposeAPITheme
 
 class MainActivity : ComponentActivity() {
@@ -19,14 +18,11 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            ComposeAPITheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) {
 
-                }
             }
         }
     }
-}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
